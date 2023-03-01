b9cbf593ce5468157dfdeffa1528df5eb6d35095
Sarthak Aggarwal <sarthakagg105@gmail.com>
2023-03-01 21:55:21 +0530

    Add files via upload

---
+++ Data Types/3. Data Types.cmd
@@ -0,0 +1,18 @@
+ABC Release 1.05.02 (central workspace).
+Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1991.
+Type '?' for help.
+>first
+>>> WRITE ("This is a number data type")
+This is a number data type
+>>> WRITE "Hello"
+Hello
+>>> WRITE ("This is a Texts data type")
+This is a Texts data type
+>>> WRITE {1..10}
+{1; 2; 3; 4; 5; 6; 7; 8; 9; 10}
+>>> WRITE ("This is a list data type")
+This is a list data type
+>>> WRITE ("Hello", 10, {1..10})
+("Hello", 10, {1; 2; 3; 4; 5; 6; 7; 8; 9; 10})
+>>> WRITE ("This is a compound Data type")
+This is a compound Data type
