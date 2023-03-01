3c8ff7f4464c918cd183437b462ec032715fd08b
Sarthak Aggarwal <sarthakagg105@gmail.com>
2023-03-01 21:25:25 +0530

    File having Printing Statements
    More Information about this is in "Information" file

---
+++ Printing Values/1. Printing Values.cmd
@@ -0,0 +1,9 @@
+ABC Release 1.05.02 (central workspace).
+Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1991.
+Type '?' for help.
+>first
+>>> W?RITE ?
+     RITE 10
+10
+>>> WRITE "Hello"
+Hello
