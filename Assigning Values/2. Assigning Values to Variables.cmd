fc7a4819afd7969ff6c41f141601b17649efccad
Sarthak Aggarwal <sarthakagg105@gmail.com>
2023-03-01 21:38:44 +0530

    Add files via upload

---
+++ Assigning Values/2. Assigning Values to Variables.cmd
@@ -0,0 +1,18 @@
+ABC Release 1.05.02 (central workspace).
+Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1991.
+Type '?' for help.
+>first
+>>> W?RITE ?
+     RITE 10
+10
+>>> WRITE "Hello"
+Hello
+>>> PUT 10 IN x
+>>> WRITE x
+10
+>>> PUT "Hello" IN y
+>>> WRITE y
+Hello
+>>> PUT {1..10} IN z
+>>> WRITE z
+{1; 2; 3; 4; 5; 6; 7; 8; 9; 10}
