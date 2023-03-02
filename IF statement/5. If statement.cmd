fcd2f84244b1c87d3121a6dd9e19a9db11632c09
Sarthak Aggarwal <sarthakagg105@gmail.com>
2023-03-02 06:40:39 +0530

    Add files via upload

---
+++ IF statement/5. If statement.cmd
@@ -0,0 +1,15 @@
+ABC Release 1.05.02 (central workspace).
+Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1991.
+Type '?' for help.
+>first
+>>> I?F ?:
+     F 100 > 10:
+       WRITE "It is TRUE"
+       \
+
+It is TRUE
+>>> IF 100 <> 10:
+       WRITE "<> Is not equal"
+       \
+
+<> Is not equal
