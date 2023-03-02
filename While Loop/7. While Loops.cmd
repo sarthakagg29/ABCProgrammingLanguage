cc41213c4e5129d2fa6dae67939aac49b1dc4455
Sarthak Aggarwal <sarthakagg105@gmail.com>
2023-03-02 07:03:15 +0530

    Add files via upload

---
+++ While Loop/7. While Loops.cmd
@@ -0,0 +1,12 @@
+ABC Release 1.05.02 (central workspace).
+Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1991.
+Type '?' for help.
+>first
+>>> W?RITE ?
+    PUT 10 IN i
+>>> WHILE i <> 20:
+       WRITE i
+       PUT i+1 IN i
+       \
+
+10 11 12 13 14 15 16 17 18 19
