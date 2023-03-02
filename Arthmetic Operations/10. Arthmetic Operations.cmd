cd3a6a54029932b94d1c1a191c3ef772ac9463a4
Sarthak Aggarwal <sarthakagg105@gmail.com>
2023-03-02 07:38:12 +0530

    Add files via upload

---
+++ Arthmetic Operations/10. Arthmetic Operations.cmd
@@ -0,0 +1,28 @@
+ABC Release 1.05.02 (central workspace).
+Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1991.
+Type '?' for help.
+>first
+>>> WRITE 2 + 3
+5
+>>> WRITE 2 - 3
+-1
+>>> WRITE 2*3
+6
+>>> WRITE 2/3
+0.6666666666666667
+>>> WRITE ~2/3
+0.6666666666666666
+>>> WRITE 2**100
+1267650600228229401496703205376
+>>> CHECK 2 < 3
+>>> CHECK 2 > 3
+*** Your check failed in your command
+    CHECK 2 > 3
+>>> CHECK 2 >= 3
+*** Your check failed in your command
+    CHECK 2 >= 3
+>>> CHECK 2 <= 3
+>>> CHECK 2 = 3
+*** Your check failed in your command
+    CHECK 2 = 3
+>>> CHECK 2 <> 3
